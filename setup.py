1775934bf6678a27745b6eff6c2aa23849403e10
H. Turgut Uyar <uyar@tekir.org>
2017-06-15 14:39:49 +0300

    Make license info consistent

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Environment :: Console',
         'Framework :: Sphinx :: Extension',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 2.7',
