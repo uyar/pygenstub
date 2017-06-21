974633ee0a7f3134380703199319fecc71f5c829
H. Turgut Uyar <uyar@tekir.org>
2017-06-21 14:28:54 +0300

    Minor update in file licensing comment

--- pygenstub.py
+++ pygenstub.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2016-2017 H. Turgut Uyar <uyar@tekir.org>
 #
+# This file is part of pygenstub.
+#
 # pygenstub is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
