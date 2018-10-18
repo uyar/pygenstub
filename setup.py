91b1a209cff9dc5e233b0a7161b139e913a94c19
H. Turgut Uyar <uyar@tekir.org>
2018-10-18 18:15:40 +0300

    admin: Apply black style

--- setup.py
+++ setup.py
@@ -3,58 +3,52 @@
 from setuptools import setup
 
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
 
 setup(
-    name='pygenstub',
-    version='1.1',
-    description='Python stub file generator.',
+    name="pygenstub",
+    version="1.1",
+    description="Python stub file generator.",
     long_description=readme,
-    url='https://github.com/uyar/pygenstub',
-    author='H. Turgut Uyar',
-    author_email='uyar@tekir.org',
-    license='GPL',
+    url="https://github.com/uyar/pygenstub",
+    author="H. Turgut Uyar",
+    author_email="uyar@tekir.org",
+    license="GPL",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Framework :: Sphinx :: Extension',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Documentation :: Sphinx',
-        'Topic :: Software Development :: Documentation'
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Framework :: Sphinx :: Extension",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Documentation :: Sphinx",
+        "Topic :: Software Development :: Documentation",
     ],
-    py_modules=['pygenstub'],
-    install_requires=['docutils'],
+    py_modules=["pygenstub"],
+    install_requires=["docutils"],
     extras_require={
-        'dev': [
-            'black',
-            'flake8',
-            'flake8-isort',
-            'flake8-docstrings',
-            'readme_renderer',
-            'wheel',
-            'twine'
+        "dev": [
+            "black",
+            "flake8",
+            "flake8-isort",
+            "flake8-docstrings",
+            "readme_renderer",
+            "wheel",
+            "twine",
         ],
-        'doc': [
-            'sphinx',
-            'sphinx_rtd_theme'
-        ],
-        'test': [
-            'pytest',
-            'pytest-cov'
-        ],
+        "doc": ["sphinx", "sphinx_rtd_theme"],
+        "test": ["pytest", "pytest-cov"],
     },
     entry_points="""
         [console_scripts]
         pygenstub=pygenstub:main
-    """
+    """,
 )
