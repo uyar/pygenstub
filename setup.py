4cd3ff7a9dc1a1356473560d6a783e784eeb0d17
H. Turgut Uyar <uyar@tekir.org>
2018-10-19 17:33:12 +0300

    admin: Add py37 to supported versions

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.4",
