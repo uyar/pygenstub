dfe177d1fc87cb2e6ff0985c9ca0e480b76d1905
H. Turgut Uyar <uyar@tekir.org>
2019-01-08 11:35:48 +0300

    tests: Refactor function stub tests

--- tests/test_pygenstub.py
+++ tests/test_pygenstub.py
@@ -4,36 +4,11 @@ import logging
 import os
 import shutil
 import sys
+from io import StringIO
 
 from pkg_resources import get_distribution
 
 from pygenstub import __version__, get_stub, main
-
-
-def_template = '''
-from x import A, B
-from . import m
-
-
-def f(%(params)s):
-    """Func
-
-    :sig: (%(ptypes)s) -> %(rtype)s
-    """
-'''
-
-function_template = '''
-from x import A, B
-from . import m
-
-
-%(decorator)s
-def f(%(params)s):
-    """Func
-
-    :sig: (%(ptypes)s) -> %(rtype)s
-    """
-'''
 
 
 class_template = '''
@@ -54,193 +29,261 @@ class C%(bases)s:
 '''
 
 
-def test_version():
+def get_function(name, desc="Foo", params=None, ptypes=None, rtype=None, decorators=None):
+    code = StringIO()
+    if decorators is not None:
+        code.write("\n".join(decorators) + "\n")
+    pstr = ", ".join(params) if params is not None else ""
+    code.write("def %(name)s(%(p)s):\n" % {"name": name, "p": pstr})
+    indent = " " * 4
+    if desc:
+        code.write(indent + '"""%(desc)s\n\n' % {"desc": desc})
+        tstr = ", ".join(ptypes) if ptypes is not None else ""
+        if rtype is not None:
+            code.write(indent + ":sig: (%(t)s) -> %(rtype)s\n" % {"t": tstr, "rtype": rtype})
+        code.write(indent + '"""\n')
+    code.write(indent + "pass\n")
+    return code.getvalue()
+
+
+def test_version_should_be_same_as_installed():
     assert get_distribution("pygenstub").version == __version__
 
 
-def test_get_stub_no_docstring():
-    code = """def f():\n    pass\n"""
+def test_if_no_docstring_then_stub_should_be_empty():
+    code = get_function("f", desc="")
     assert get_stub(code) == ""
 
 
-def test_get_stub_no_sig_in_docstring():
-    code = '''def f():\n    """Func\n    """\n'''
+def test_if_no_sig_then_stub_should_be_empty():
+    code = get_function("f")
     assert get_stub(code) == ""
 
 
-def test_get_stub_two_functions_only_one_sig():
-    code = (
-        def_template % {"params": "", "ptypes": "", "rtype": "None"}
-        + '''def g():\n    """Func\n    """\n'''
-    )
+def test_if_returns_none_then_stub_should_return_none():
+    code = get_function("f", rtype="None")
     assert get_stub(code) == "def f() -> None: ...\n"
 
 
-def test_get_stub_params_none_returns_none():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "None"}
-    assert get_stub(code) == "def f() -> None: ...\n"
-
-
-def test_get_stub_returns_builtin():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "int"}
+def test_if_returns_builtin_then_stub_should_return_builtin():
+    code = get_function("f", rtype="int")
     assert get_stub(code) == "def f() -> int: ...\n"
 
 
-def test_get_stub_returns_imported():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "A"}
+def test_if_returns_from_imported_then_stub_should_include_import():
+    code = "from x import A\n"
+    code += "\n\n" + get_function("f", rtype="A")
     assert get_stub(code) == "from x import A\n\ndef f() -> A: ...\n"
 
 
-def test_get_stub_returns_dotted():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "r.s.T"}
-    assert get_stub(code) == "import r.s\n\ndef f() -> r.s.T: ...\n"
+def test_stub_should_exclude_unused_import():
+    code = "from x import A, B\n"
+    code += "\n\n" + get_function("f", rtype="A")
+    assert get_stub(code) == "from x import A\n\ndef f() -> A: ...\n"
 
 
-def test_get_stub_returns_dotted_imported():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "A.X"}
-    assert get_stub(code) == "from x import A\n\ndef f() -> A.X: ...\n"
+# def test_stub_should_include_all_used_imports():
+#     code = "from x import A, B\n"
+#     code += "\n\n" + get_function("f", rtype="A")
+#     code += "\n\n" + get_function("g", rtype="B")
+#     assert get_stub(code) == "from x import A, B\n\ndef f() -> A: ...\ndef g() -> B: ...\n"
 
 
-def test_get_stub_returns_dotted_imported_relative():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "m.X"}
-    assert get_stub(code) == "from . import m\n\ndef f() -> m.X: ...\n"
+def test_if_returns_imported_qualified_then_stub_should_include_import():
+    code = "import x\n"
+    code += "\n\n" + get_function("f", rtype="x.A")
+    assert get_stub(code) == "import x\n\ndef f() -> x.A: ...\n"
 
 
-def test_get_stub_returns_typing():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "List"}
+def test_if_returns_from_imported_qualified_then_stub_should_include_import():
+    code = "from x import y\n"
+    code += "\n\n" + get_function("f", rtype="y.A")
+    assert get_stub(code) == "from x import y\n\ndef f() -> y.A: ...\n"
+
+
+def test_if_returns_module_imported_qualified_then_stub_should_include_import():
+    code = "from x import y\n"
+    code += get_function("f", rtype="y.A")
+    assert get_stub(code) == "from x import y\n\ndef f() -> y.A: ...\n"
+
+
+def test_if_returns_relative_imported_then_stub_should_include_import():
+    code = "from . import x\n"
+    code += "\n\n" + get_function("f", rtype="x.A")
+    assert get_stub(code) == "from . import x\n\ndef f() -> x.A: ...\n"
+
+
+def test_if_returns_unimported_qualified_then_stub_should_generate_import():
+    code = get_function("f", rtype="x.y.A")
+    assert get_stub(code) == "import x.y\n\ndef f() -> x.y.A: ...\n"
+
+
+def test_if_returns_imported_typing_then_stub_should_include_import():
+    code = "from typing import List\n"
+    code += "\n\n" + get_function("f", rtype="List")
     assert get_stub(code) == "from typing import List\n\ndef f() -> List: ...\n"
 
 
-def test_get_stub_returns_typing_qualified():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "List[str]"}
+def test_if_returns_unimported_typing_then_stub_should_generate_import():
+    code = get_function("f", rtype="List")
+    assert get_stub(code) == "from typing import List\n\ndef f() -> List: ...\n"
+
+
+def test_if_returns_qualified_typing_then_stub_should_return_qualified_typing():
+    code = get_function("f", rtype="List[str]")
     assert get_stub(code) == "from typing import List\n\ndef f() -> List[str]: ...\n"
 
 
-def test_get_stub_returns_typing_qualified_multiple():
-    code = def_template % {"params": "", "ptypes": "", "rtype": "Dict[str, Any]"}
+def test_if_returns_multiple_typing_then_stub_should_generate_multiple_import():
+    code = get_function("f", rtype="Dict[str, Any]")
     assert get_stub(code) == "from typing import Any, Dict\n\ndef f() -> Dict[str, Any]: ...\n"
 
 
-def test_get_stub_missing_name():
-    code = def_template % {"params": "i", "ptypes": "foo", "rtype": "Dict[str, Any]"}
+def test_if_returns_unknown_type_should_raise_error():
+    code = get_function("f", rtype="Foo")
     with raises(RuntimeError) as e:
         get_stub(code)
-    assert "Unknown types: foo" in str(e)
+    assert "Unknown types: Foo" in str(e)
 
 
-def test_get_stub_params_one_builtin():
-    code = def_template % {"params": "i", "ptypes": "int", "rtype": "None"}
+def test_if_one_param_then_stub_should_have_one_param():
+    code = get_function("f", params=["i"], ptypes=["int"], rtype="None")
     assert get_stub(code) == "def f(i: int) -> None: ...\n"
 
 
-def test_get_stub_params_two_builtin():
-    code = def_template % {"params": "i, s", "ptypes": "int, str", "rtype": "None"}
+def test_if_multiple_params_then_stub_should_have_multiple_params():
+    code = get_function("f", params=["i", "s"], ptypes=["int", "str"], rtype="None")
     assert get_stub(code) == "def f(i: int, s: str) -> None: ...\n"
 
 
-def test_get_stub_params_imported():
-    code = def_template % {"params": "a", "ptypes": "A", "rtype": "None"}
+def test_if_param_type_imported_then_stub_should_include_import():
+    code = "from x import A\n"
+    code += "\n\n" + get_function("f", params=["a"], ptypes=["A"], rtype="None")
     assert get_stub(code) == "from x import A\n\ndef f(a: A) -> None: ...\n"
 
 
-def test_get_stub_params_dotted():
-    code = def_template % {"params": "a", "ptypes": "x.A", "rtype": "None"}
+def test_if_param_type_imported_qualified_then_stub_should_include_import():
+    code = "import x\n"
+    code += "\n\n" + get_function("f", params=["a"], ptypes=["x.A"], rtype="None")
     assert get_stub(code) == "import x\n\ndef f(a: x.A) -> None: ...\n"
 
 
-def test_get_stub_params_dotted_imported():
-    code = def_template % {"params": "a", "ptypes": "A.X", "rtype": "None"}
-    assert get_stub(code) == "from x import A\n\ndef f(a: A.X) -> None: ...\n"
+def test_if_param_type_from_imported_qualified_then_stub_should_include_import():
+    code = "from x import A\n"
+    code += "\n\n" + get_function("f", params=["a"], ptypes=["A"], rtype="None")
+    assert get_stub(code) == "from x import A\n\ndef f(a: A) -> None: ...\n"
 
 
-def test_get_stub_params_dotted_imported_relative():
-    code = def_template % {"params": "a", "ptypes": "m.A", "rtype": "None"}
-    assert get_stub(code) == "from . import m\n\ndef f(a: m.A) -> None: ...\n"
+def test_if_param_type_module_imported_qualified_then_stub_should_include_import():
+    code = "from x import y\n"
+    code += get_function("f", params=["a"], ptypes=["y.A"], rtype="None")
+    assert get_stub(code) == "from x import y\n\ndef f(a: y.A) -> None: ...\n"
 
 
-def test_get_stub_params_typing():
-    code = def_template % {"params": "l", "ptypes": "List", "rtype": "None"}
+def test_if_param_type_relative_imported_qualified_then_stub_should_include_import():
+    code = "from . import x\n"
+    code += "\n\n" + get_function("f", params=["a"], ptypes=["x.A"], rtype="None")
+    assert get_stub(code) == "from . import x\n\ndef f(a: x.A) -> None: ...\n"
+
+
+def test_if_param_type_unimported_qualified_then_stub_should_generate_import():
+    code = get_function("f", params=["a"], ptypes=["x.y.A"], rtype="None")
+    assert get_stub(code) == "import x.y\n\ndef f(a: x.y.A) -> None: ...\n"
+
+
+def test_if_param_type_imported_typing_then_stub_should_include_import():
+    code = "from typing import List\n"
+    code += "\n\n" + get_function("f", params=["l"], ptypes=["List"], rtype="None")
     assert get_stub(code) == "from typing import List\n\ndef f(l: List) -> None: ...\n"
 
 
-def test_get_stub_params_typing_qualified():
-    code = def_template % {"params": "i, l", "ptypes": "int, List[str]", "rtype": "None"}
+def test_if_param_type_unimported_typing_then_stub_should_include_import():
+    code = get_function("f", params=["l"], ptypes=["List"], rtype="None")
+    assert get_stub(code) == "from typing import List\n\ndef f(l: List) -> None: ...\n"
+
+
+def test_if_param_type_qualified_typing_then_stub_should_include_qualified_typing():
+    code = get_function("f", params=["ls"], ptypes=["List[str]"], rtype="None")
+    assert get_stub(code) == "from typing import List\n\ndef f(ls: List[str]) -> None: ...\n"
+
+
+def test_if_param_type_multiple_typing_then_stub_should_include_multiple_import():
+    code = get_function("f", params=["d"], ptypes=["Dict[str, Any]"], rtype="None")
     assert (
         get_stub(code)
-        == "from typing import List\n\ndef f(i: int, l: List[str]) -> None: ...\n"
+        == "from typing import Any, Dict\n\ndef f(d: Dict[str, Any]) -> None: ...\n"
     )
 
 
-def test_get_stub_params_typing_qualified_multiple():
-    code = def_template % {"params": "i, d", "ptypes": "int, Dict[str, Any]", "rtype": "None"}
+def test_if_param_type_unknown_should_raise_error():
+    code = get_function("f", params=["i"], ptypes=["Foo"], rtype="None")
+    with raises(RuntimeError) as e:
+        get_stub(code)
+    assert "Unknown types: Foo" in str(e)
+
+
+def test_if_param_has_default_then_stub_should_include_ellipses():
+    code = get_function("f", params=["i=0"], ptypes=["Optional[int]"], rtype="None")
     assert (
         get_stub(code)
-        == "from typing import Any, Dict\n\ndef f(i: int, d: Dict[str, Any]) -> None: ...\n"
+        == "from typing import Optional\n\ndef f(i: Optional[int] = ...) -> None: ...\n"
     )
 
 
-def test_get_stub_params_typing_and_imported():
-    code = def_template % {"params": "l, a", "ptypes": "List, A", "rtype": "None"}
-    assert (
-        get_stub(code)
-        == "from typing import List\n\nfrom x import A\n\ndef f(l: List, a: A) -> None: ...\n"
-    )
-
-
-def test_get_stub_params_default_value():
-    code = def_template % {"params": "s, i=0", "ptypes": "str, Optional[int]", "rtype": "None"}
-    assert (
-        get_stub(code)
-        == "from typing import Optional\n\ndef f(s: str, i: Optional[int] = ...) -> None: ...\n"
-    )
-
-
-def test_get_stub_params_vararg():
-    code = def_template % {"params": "i, *args", "ptypes": "int", "rtype": "None"}
+def test_stub_should_ignore_varargs_type():
+    code = get_function("f", params=["i", "*args"], ptypes=["int"], rtype="None")
     assert get_stub(code) == "def f(i: int, *args) -> None: ...\n"
 
 
-def test_get_stub_params_kwargs():
-    code = def_template % {"params": "i, **kwargs", "ptypes": "int", "rtype": "None"}
+def test_stub_should_ignore_kwargs_type():
+    code = get_function("f", params=["i", "**kwargs"], ptypes=["int"], rtype="None")
     assert get_stub(code) == "def f(i: int, **kwargs) -> None: ...\n"
 
 
-def test_get_stub_params_vararg_and_kwargs():
-    code = def_template % {"params": "i, *args, **kwargs", "ptypes": "int", "rtype": "None"}
+def test_stub_should_ignore_vararg_and_kwargs_types():
+    code = get_function("f", params=["i", "*args", "**kwargs"], ptypes=["int"], rtype="None")
     assert get_stub(code) == "def f(i: int, *args, **kwargs) -> None: ...\n"
 
 
 @mark.skipif(sys.version_info < (3, 0), reason="syntax introduced in py3")
-def test_get_stub_params_kwonly_args():
-    code = def_template % {"params": "i, *, j", "ptypes": "int, int", "rtype": "None"}
+def test_stub_should_honor_kwonly_args():
+    code = get_function("f", params=["i", "*", "j"], ptypes=["int", "int"], rtype="None")
     assert get_stub(code) == "def f(i: int, *, j: int) -> None: ...\n"
 
 
 @mark.skipif(sys.version_info < (3, 0), reason="syntax introduced in py3")
-def test_get_stub_params_kwonly_args_with_default():
-    code = def_template % {
-        "params": "i, *, j=0",
-        "ptypes": "int, Optional[int]",
-        "rtype": "None",
-    }
+def test_stub_should_honor_kwonly_args_with_default():
+    code = get_function(
+        "f", params=["i", "*", "j=0"], ptypes=["int", "Optional[int]"], rtype="None"
+    )
     assert (
         get_stub(code)
         == "from typing import Optional\n\ndef f(i: int, *, j: Optional[int] = ...) -> None: ...\n"
     )
 
 
-def test_get_stub_params_missing_types():
-    code = def_template % {"params": "i, j", "ptypes": "int", "rtype": "None"}
+def test_missing_types_should_raise_error():
+    code = get_function("f", params=["i", "j"], ptypes=["int"], rtype="None")
     with raises(RuntimeError) as e:
         get_stub(code)
     assert "Parameter names and types don't match: " in str(e)
 
 
-def test_get_stub_params_extra_types():
-    code = def_template % {"params": "i", "ptypes": "int, int", "rtype": "None"}
+def test_extra_types_should_raise_error():
+    code = get_function("f", params=["i"], ptypes=["int", "int"], rtype="None")
     with raises(RuntimeError) as e:
         get_stub(code)
     assert "Parameter names and types don't match: " in str(e)
+
+
+def test_if_function_decorated_unknown_then_stub_should_ignore():
+    code = get_function("f", rtype="None", decorators=["@foo"])
+    assert get_stub(code) == "def f() -> None: ...\n"
+
+
+def test_if_function_decorated_callable_unknown_then_stub_should_ignore():
+    code = get_function("f", rtype="None", decorators=["@foo()"])
+    assert get_stub(code) == "def f() -> None: ...\n"
 
 
 def test_get_stub_method_self():
@@ -390,26 +433,6 @@ def test_get_stub_comment_instance_variable():
     )
 
 
-def test_get_stub_function_decorated_unknown():
-    code = function_template % {
-        "decorator": "@foo",
-        "params": "",
-        "ptypes": "",
-        "rtype": "None",
-    }
-    assert get_stub(code) == "def f() -> None: ...\n"
-
-
-def test_get_stub_function_decorated_unknown_with_parameters():
-    code = function_template % {
-        "decorator": "@foo()",
-        "params": "",
-        "ptypes": "",
-        "rtype": "None",
-    }
-    assert get_stub(code) == "def f() -> None: ...\n"
-
-
 def test_get_stub_method_decorated_unknown():
     code = class_template % {
         "bases": "",
@@ -473,6 +496,21 @@ def test_get_stub_method_decorated_property_setter():
         "comment": "",
     }
     assert get_stub(code) == "class C:\n    @x.setter\n    def m(self) -> None: ...\n"
+
+
+def test_stub_should_exclude_function_without_sig():
+    code = get_function("f", rtype="None")
+    code += "\n\n" + get_function("g", desc="")
+    assert get_stub(code) == "def f() -> None: ...\n"
+
+
+def test_get_stub_typing_import_should_come_first():
+    code = "from x import A\n"
+    code += "\n\n" + get_function("f", params=["a", "l"], ptypes=["A", "List"], rtype="None")
+    assert (
+        get_stub(code)
+        == "from typing import List\n\nfrom x import A\n\ndef f(a: A, l: List) -> None: ...\n"
+    )
 
 
 ########################################
