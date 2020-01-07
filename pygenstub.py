14b572076a96aa7f295650f2f5b738c6d8ba72f5
H. Turgut Uyar <uyar@tekir.org>
2020-01-07 19:58:41 +0300

    Shorten module docstring

--- pygenstub.py
+++ pygenstub.py
@@ -13,14 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pygenstub.  If not, see <http://www.gnu.org/licenses/>.
 
-"""pygenstub is a utility for generating stub files from docstrings in source files.
-
-It takes a source file as input and creates a stub file with the same base name
-and the ``.pyi`` extension.
-
-For more information, please refer to the documentation:
-https://pygenstub.tekir.org/
-"""
+"""A utility for generating stub files from docstrings in source files."""
 
 from __future__ import absolute_import, print_function, unicode_literals
 
