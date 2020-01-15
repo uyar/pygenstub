0ccb01f0cbfa9de60204099c779a7f365137a840
H. Turgut Uyar <uyar@tekir.org>
2020-01-15 18:12:09 +0300

    Add tests for signature parsing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from pytest import raises
 
-from pygenstub import extract_signature, get_mod_paths, get_pkg_paths
+from pygenstub import extract_signature, get_mod_paths, get_pkg_paths, parse_signature
 
 
 def test_extract_signature_should_return_value_of_sig_field():
@@ -14,6 +14,64 @@ def test_extract_signature_should_return_none_if_no_sig_field():
 def test_extract_signature_should_fail_if_multiple_sig_fields():
     with raises(ValueError):
         extract_signature("foo\n\n:sig: () -> None\n:sig: () -> None\n")
+
+
+def test_parse_signature_should_return_input_types_return_type_and_required_types():
+    assert parse_signature("(str) -> int") == (["str"], "int", {"str", "int"})
+
+
+def test_parse_signature_should_return_multiple_input_types_in_sequence():
+    assert parse_signature("(str, bool) -> int")[0] == ["str", "bool"]
+
+
+def test_parse_signature_should_return_all_input_types_if_duplicates():
+    assert parse_signature("(str, str) -> int")[0] == ["str", "str"]
+
+
+def test_parse_signature_should_treat_bracketed_types_as_a_unit():
+    assert parse_signature("(List[str]) -> int")[0] == ["List[str]"]
+
+
+def test_parse_signature_should_treat_bracketed_csv_types_as_a_unit():
+    assert parse_signature("(int, Tuple[str, int]) -> int")[0] == ["int", "Tuple[str, int]"]
+
+
+def test_parse_signature_should_treat_nested_bracketed_types_as_a_unit():
+    assert parse_signature("(int, Optional[Tuple[str, int]], str) -> int")[0] == [
+        "int",
+        "Optional[Tuple[str, int]]",
+        "str",
+    ]
+
+
+def test_parse_signature_should_return_empty_input_types_if_no_input_parameters():
+    assert parse_signature("() -> int")[0] == []
+
+
+def test_parse_signature_should_accept_none_as_a_type():
+    assert parse_signature("() -> None") == ([], "None", {"None"})
+
+
+def test_parse_signature_should_return_non_duplicated_required_types():
+    assert parse_signature("(str, str) -> int")[2] == {"str", "int"}
+
+
+def test_parse_signature_should_consider_components_of_bracketed_types_as_separate():
+    assert parse_signature("(str, List[str]) -> None")[2] == {"str", "List", "None"}
+
+
+def test_parse_signature_should_consider_components_of_nested_bracketed_types_as_separate():
+    assert parse_signature("(int, Optional[Tuple[str, int]], str) -> Tuple[int, bool]")[2] == {
+        "int",
+        "Optional",
+        "Tuple",
+        "str",
+        "bool",
+    }
+
+
+def test_parse_signature_should_treat_signature_as_variable_type_comment_if_no_arrow():
+    assert parse_signature("int") == (None, "int", {"int"})
 
 
 def test_get_mod_source_should_return_python_file_path():
